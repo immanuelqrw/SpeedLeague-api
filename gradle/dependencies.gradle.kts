9738c9cd2def46a8e2535fcc6d1d78687a98fda1
Immanuel Washington <code.immanuelqrw@gmail.com>
2019-09-08 20:13:01 -0400

    Search Fix

--- gradle/dependencies.gradle.kts
+++ gradle/dependencies.gradle.kts
@@ -25,7 +25,7 @@ val nucleusPackages: Array<Package> = arrayOf(
     Package("com.immanuelqrw.core", "nucleus-util", nucleusVersion),
     Package("com.immanuelqrw.core", "nucleus-test", nucleusVersion),
     Package("com.immanuelqrw.core", "nucleus-entity", "0.0.13-pre-alpha"),
-    Package("com.immanuelqrw.core", "nucleus-api", "0.0.21-pre-alpha")
+    Package("com.immanuelqrw.core", "nucleus-api", "0.0.23-pre-alpha")
 )
 
 val nucleusTestPackages: Array<Package> = arrayOf(
