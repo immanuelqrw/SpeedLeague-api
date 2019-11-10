89d87222126ea30170e5371383c5a506f9768053
Immanuel Washington <code.immanuelqrw@gmail.com>
2019-11-09 23:00:09 -0500

    No auto methods for Spring
    Due to using findAllActive, auto-methods shouldn't be used

--- src/main/kotlin/com/immanuelqrw/speedleague/api/service/seek/GameSeekService.kt
+++ src/main/kotlin/com/immanuelqrw/speedleague/api/service/seek/GameSeekService.kt
@@ -10,15 +10,12 @@ import javax.persistence.EntityNotFoundException
 @Service
 class GameSeekService : BaseUniqueService<Game>(Game::class.java) {
 
-    @Autowired
-    private lateinit var gameRepository: GameRepository
-
     fun findByName(name: String): Game {
         return findAllActive(search = "name:$name").firstOrNull() ?: throw EntityNotFoundException()
     }
 
     fun findByShorthand(shorthand: String): Game {
-        return gameRepository.findByShorthand(shorthand)
+        return findAllActive(search = "shorthand:$shorthand").firstOrNull() ?: throw EntityNotFoundException()
     }
 
 }
